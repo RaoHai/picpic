2ceec8a5f4c46a3b64acdc776d7039006264bf69
blackbookfire <573633611@qq.com>
2011-10-03 15:36:52 +0800

    Signed-off-by: blackbookfire <573633611@qq.com> 用户界面功能完善中.........

--- loginin.php
+++ loginin.php
@@ -1,14 +1,24 @@
 <?php
+require_once('handler.php');
+session_start();
+$userid=$_POST['userid'];
+$birthday=$_POST['birthday'];
+$profile=$imgView->Getprofile($userid);
 $click1=$_GET['click1'];
-if($click1==NULL)
 	echo<<<EOD
 <div>
 <form name="login" action="loginin.php" method="get" >
-<input type='button' name='click1' value='0' href=# onclick='document.login.submit'/>  
+	<input type=submit name='click1' value='用户'/>  
 </form>
 </div>
 
 EOD;
-if($click1=='0')
-	echo "123";
+if($click1=='用户')
+	echo<<<EOD
+<div>
+<form name="user" action="loginin.php" method="post">
+	<span>添加图片...</span>
+	<p>昵称：<input type="text" name='userid' ></p>
+	<p>生日：<input type="text" name='birthday'></p>
+EOD;
 ?>
