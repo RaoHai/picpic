1fdec9d2cc92441e2f4bf4aa229153a5c17fa7c6
surgesoft <surgesoft@gmail.com>
2012-05-01 19:25:00 +0800

    premergeing Signed-off-by: surgesoft <surgesoft@gmail.com>

--- htdocs/demo/db_connect.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$user = "vogoo" ;
-$passwd = "" ;
-$db_name = "vogoo_web" ;
-$db = mysql_connect("localhost",$user,$passwd) ;
-mysql_select_db($db_name) ;
-
-?>
--- htdocs/demo/vogoo_bkp/vogoo-2.2/vogoo/index.php
+++
@@ -1,6 +0,0 @@
-<?php
-	include_once("vogoo.php");
-	echo "hello world";
-	echo $vogoo->connected;
-	$vogoo->set_rating(1,1,1,1);
-?>
